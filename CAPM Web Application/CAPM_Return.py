9f834b56eccf547cc90b22c595708f770a1cf7af
Shantnu Kumar <141835400+Shantnu-K@users.noreply.github.com>
2023-08-15 22:14:22 +0530

    Add files via upload
    A web application that is designed to perform CAPM calculations for different stocks. The application uses Python programming language and its libraries such as Pandas, NumPy, Streamlit, and Plotly to gather stock data from Yahoo Finance and perform calculations to determine expected returns.

---
+++ CAPM Web Application/CAPM_Return.py
@@ -0,0 +1,107 @@
+
+
+# Import libraries
+import streamlit as st
+import pandas as pd
+import yfinance as yf
+import datetime
+import pandas_datareader.data as web
+import capm_functions
+
+# Set page configuration for Streamlit app
+st.set_page_config(
+    page_title="CAPM",
+    page_icon="chart_with_upward_trend",
+    layout="wide"
+)
+
+# Display the app title
+st.title("Capital Asset Pricing Model")
+
+# Get user input for stock selection and number of years
+col1, col2 = st.columns([1, 1])
+with col1:
+    stocks_list = st.multiselect("Choose 4 stocks", ('TSLA', 'AAPL', 'NFLX', 'MSFT', 'MGM', 'AMZN', 'NVDA', 'GOOGL'),
+                                 ['TSLA', 'AAPL', 'AMZN', 'GOOGL'])
+with col2:
+    year = st.number_input("Number of years", 1, 10)
+
+try:
+    # Download data for S&P 500
+    end = datetime.date.today()
+    start = datetime.date(datetime.date.today().year - year, datetime.date.today().month, datetime.date.today().day)
+    SP500 = web.DataReader(['sp500'], 'fred', start, end)
+
+    # Download stock price data for selected stocks
+    stocks_df = pd.DataFrame()
+    for stock in stocks_list:
+        data = yf.download(stock, period=f'{year}y')
+        stocks_df[f'{stock}'] = data['Close']
+
+    # Prepare data for visualization
+    stocks_df.reset_index(inplace=True)
+    SP500.reset_index(inplace=True)
+    SP500.columns = ['Date', 'sp500']
+    stocks_df['Date'] = stocks_df['Date'].astype('datetime64[ns]')
+    stocks_df['Date'] = stocks_df['Date'].apply(lambda x: str(x)[:10])
+    stocks_df['Date'] = pd.to_datetime(stocks_df['Date'])
+    stocks_df = pd.merge(stocks_df, SP500, on='Date', how='inner')
+
+    # Display head and tail of the DataFrame
+    col1, col2 = st.columns([1, 1])
+    with col1:
+        st.markdown("### Dataframe head")
+        st.dataframe(stocks_df.head(), use_container_width=True)
+    with col2:
+        st.markdown("### Dataframe tail")
+        st.dataframe(stocks_df.tail(), use_container_width=True)
+
+    # Display stock prices before and after normalization
+    col1, col2 = st.columns([1, 1])
+    with col1:
+        st.markdown("### Price of all the stocks")
+        st.plotly_chart(capm_functions.interactive_plot(stocks_df))
+    with col2:
+        st.markdown("### Price of all the stocks (After Normalization)")
+        st.plotly_chart(capm_functions.interactive_plot(capm_functions.normalize(stocks_df)))
+
+    # Calculate and display beta values
+    stocks_daily_return = capm_functions.daily_return(stocks_df)
+    beta = {}
+    alpha = {}
+
+    for i in stocks_daily_return.columns:
+        if i != 'Date' and i != 'sp500':
+            b, a = capm_functions.calculate_beta(stocks_daily_return, i)
+            beta[i] = b
+            alpha[i] = a
+
+    # Display calculated beta values
+    col1, col2 = st.columns([1, 1])
+    beta_df = pd.DataFrame(columns=['stock', 'Beta Value'])
+    beta_df['Stock'] = beta.keys()
+    beta_df['Beta Value'] = [str(round(i, 2)) for i in beta.values()]
+
+    with col1:
+        st.markdown('### Calculated Beta Value')
+        st.dataframe(beta_df, use_container_width=True)
+
+    # Calculate and display returns using CAPM
+    rf = 0
+    rm = stocks_daily_return['sp500'].mean() * 252
+    return_df = pd.DataFrame()
+    return_value = []
+
+    for stock, value in beta.items():
+        return_value.append(str(round(rf + (value * (rm - rf)), 2)))
+
+    return_df['Stock'] = stocks_list
+    return_df['Return Value'] = return_value
+
+    with col2:
+        st.markdown('### Calculated Return using CAPM')
+        st.dataframe(return_df, use_container_width=True)
+
+except:
+    # Display an error message if input is invalid
+    st.write("Please select valid input")
